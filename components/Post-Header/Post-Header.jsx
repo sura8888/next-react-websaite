c4a49f5df50f987d6e510f389ce8bf976c13b682
sura8888 <104048527+sura8888@users.noreply.github.com>
2023-02-10 16:01:49 +0900

    2/10

--- components/Post-Header/Post-Header.jsx
+++ components/Post-Header/Post-Header.jsx
@@ -4,6 +4,8 @@ export default function PostHeader({ title, subtitle, publish = ''}) {
     return (
         <div className={styles.stack}>
             <p className={styles.subtitle}>{subtitle}</p>
+            <h1 className={styles.title}>{title}</h1>
+            {publish && <div className={styles.publish}>{publish}</div>}
         </div>
     )
 }
